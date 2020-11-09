f1ed3b7f89d32446584fb50bc01a4cc3f458309f
Mateus Cappellari Vieira <mateusv@email.com>
2020-11-09 17:21:16 -0300

    std indent for all models, use 4 spaces

--- src/app/models/Video.js
+++ src/app/models/Video.js
@@ -3,21 +3,21 @@ import Sequelize, { Model } from 'sequelize';
 class Video extends Model {
     // Parâmetro de entrada é a conexão do model
     static init(sequelize) {
-      // Iniciando classe pai de user (Model)
+        // Iniciando classe pai de user (Model)
         super.init(
-        {
-            // definição das colunas (sem PK, FK e Create/Update)
-            title: Sequelize.STRING,
-            file: Sequelize.STRING,
-            description: Sequelize.STRING,
-            timestamp: Sequelize.DATE,
-        },
-        {
-            sequelize,
-        }
-      );
+            {
+                // definição das colunas (sem PK, FK e Create/Update)
+                title: Sequelize.STRING,
+                file: Sequelize.STRING,
+                description: Sequelize.STRING,
+                timestamp: Sequelize.DATE,
+            },
+            {
+                sequelize,
+            }
+        );
     }
 
 }
-  
+
 export default Video;
