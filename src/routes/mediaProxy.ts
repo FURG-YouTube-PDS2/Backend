28a1d8ef7883770235ece7b62ffb175b251ed7ff
Thiago <thhiaggo1@hotmail.com>
2021-02-21 11:00:59 -0300

    Proxy and register with avatar done

--- src/routes/mediaProxy.ts
+++ src/routes/mediaProxy.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import aws from "aws-sdk";
-import awsConfig from '../config/aws';
+
 import { getRepository } from 'typeorm';
 import Video from '../models/Video';
 import User from '../models/User';
 import UserVideo from '../models/UserVideo';
 import checkJwt from '../middlewares/checkJwt';
+import aws from 'aws-sdk';
+import awsConfig from '../config/aws';
 const mediaProxy = Router();
 
 const s3 = new aws.S3({
@@ -19,73 +20,87 @@ mediaProxy.get('/*', async (req, res) => {
 	try {
 		//get path after /media
 		var file = req.params[0];
-		var type = file.split("/")[0];
-		console.log(file)
+		var type = file.split('/')[0];
+		console.log(file);
 
-		if(type == "image"){
+		if (type == 'image') {
 			const userRepository = getRepository(User);
-			const user = await userRepository.findOne({ where: { avatar: file} });
-			if(user){
+			const user = await userRepository.findOne({ where: { avatar: file } });
+			if (user) {
 				//connect to bucket and get file
-				var imgStream = s3.getObject({
-					Bucket: aws_bucket,
-					Key: file
-				}).createReadStream();
+				var imgStream = s3
+					.getObject({
+						Bucket: aws_bucket,
+						Key: file,
+					})
+					.createReadStream();
 				//pipe file to response
 				imgStream.pipe(res);
 			}
 		}
 
-		if (type === "video" && req.headers.authorization) {
+		if (type === 'video' && req.headers.authorization) {
 			const token = req.headers.authorization;
 			var user_id = checkJwt(token).sub;
-			
+
 			//privacy verification (private) below
-		
+
 			const videoRepository = getRepository(Video);
 			const video = await videoRepository.findOne({ where: { file } });
 			const userVideoRepository = getRepository(UserVideo);
-			const userVideo = await userVideoRepository.findOne({ where: [{ user_id, video_id:video?.id} /* tumb test */]});
-			
-			if(video && video?.private && userVideo && userVideo?.is_owner){
+			const userVideo = await userVideoRepository.findOne({
+				where: [{ user_id, video_id: video?.id } /* tumb test */],
+			});
+
+			if (video && video?.private && userVideo && userVideo?.is_owner) {
 				//connect to bucket and get file
-				var imgStream = s3.getObject({
-					Bucket: aws_bucket,
-					Key: file
-				}).createReadStream();
+				var imgStream = s3
+					.getObject({
+						Bucket: aws_bucket,
+						Key: file,
+					})
+					.createReadStream();
 				//pipe file to response
 				imgStream.pipe(res);
 			}
 
-			if(video && !video?.private){
+			if (video && !video?.private) {
 				//connect to bucket and get file
-				var imgStream = s3.getObject({
-					Bucket: aws_bucket,
-					Key: file
-				}).createReadStream();
+				var imgStream = s3
+					.getObject({
+						Bucket: aws_bucket,
+						Key: file,
+					})
+					.createReadStream();
 				//pipe file to response
 				imgStream.pipe(res);
 			}
+		}
 
+		if (type == 'nintube') {
+			//connect to bucket and get file
+			var imgStream = s3
+				.getObject({
+					Bucket: aws_bucket,
+					Key: file,
+				})
+				.createReadStream();
+			//pipe file to response
+			imgStream.pipe(res);
+		}
 
-			
-			
-		} 
-		
 		//connect to bucket and get file
-		var imgStream = s3.getObject({
-			Bucket: aws_bucket,
-			Key: "nintube/404.png"
-		}).createReadStream();
+		var imgStream = s3
+			.getObject({
+				Bucket: aws_bucket,
+				Key: 'nintube/404.png',
+			})
+			.createReadStream();
 		//pipe file to response
 		imgStream.pipe(res);
 	} catch (err) {
 		res.status(400).json({ status: 0, errorName: err, errorMessage: err.message });
 	}
-
 });
-
-
-
 
 export default mediaProxy;
--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,20 @@ import express from 'express';
 import 'reflect-metadata';
 import './database';
 import routes from './routes';
-import "reflect-metadata";
-import cors from "cors";
+import 'reflect-metadata';
+// import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import awsConfig from './config/aws';
 
-
-
 const app = express();
-
+var bodyParser = require('body-parser');
 app.use(cors());
-
-
+app.use(bodyParser.json({ limit: '100mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 app.use(express.json());
 app.use(routes);
 
 app.listen(3334, () => {
 	console.log('Server started on port 3334');
 });
-
