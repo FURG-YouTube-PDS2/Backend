9b865c35d1c925896f923c46f0212ecd28f6083c
Thiago <thhiaggo1@hotmail.com>
2021-04-28 18:31:11 -0300

    statistic

--- src/services/statistic/statisticChannelService.ts
+++ src/services/statistic/statisticChannelService.ts
@@ -15,86 +15,162 @@ interface Request {
 class statisticChannelService {
 	public async execute({ token }: Request): Promise<object> {
 		try {
-			const videoRepository = getRepository(Video);
+			const subsRepo = getRepository(Subscription);
 			const userRepository = getRepository(User);
 			const userVideoRepository = getRepository(UserVideo);
-			const user_id = checkJwt(token).sub;
-			const today = new Date();
-			const year = today.getFullYear();
 
-			if (videoRepository && userRepository && userVideoRepository) {
-				const videos_id = await userVideoRepository.find({
+			const user_id = checkJwt(token).sub;
+
+			if (userRepository && userVideoRepository) {
+				const ids = await userVideoRepository.find({
 					select: ['video_id'],
 					where: {
 						user_id,
 						is_owner: true,
 					},
 				});
-				const users_id = await userVideoRepository
+				var videos_id = new Array();
+				for (let i = 0; i < ids.length; i++) {
+					videos_id.push(ids[i].video_id);
+				}
+
+				const u_id = await userVideoRepository
 					.createQueryBuilder('user_videos')
 					.select('user_id')
-					.where('is_owner != true AND video_id IN (:videos_id)', { videos_id })
+					.where(' video_id IN (:...videos_id)', { videos_id })
 					.getRawMany();
+				var users_id = new Array();
+				for (let i = 0; i < u_id.length; i++) {
+					if (!users_id.includes(u_id[i].user_id)) {
+						users_id.push(u_id[i].user_id);
+					}
+				}
+
 				const female = await userRepository
 					.createQueryBuilder('user')
 					.select('gender')
-					.where('gender = "f" AND id IN (:users_id)', { user_id })
+					.where('gender = (:g) AND id IN (:...users_id)', { g: 'w', users_id })
 					.getCount();
+
 				const male = await userRepository
 					.createQueryBuilder('user')
 					.select('gender')
-					.where('gender = "m" AND id IN (:users_id)', { user_id })
+					.where('gender = (:g) AND id IN (:...users_id)', { g: 'm', users_id })
 					.getCount();
 				const others = await userRepository
 					.createQueryBuilder('user')
 					.select('gender')
-					.where('gender = "o" AND id IN (:users_id)', { user_id })
+					.where('gender = (:g) AND id IN (:...users_id)', { g: 'a', users_id })
 					.getCount();
 
 				const ten = await userRepository
 					.createQueryBuilder('user')
 					.select('birthdate')
-					.where('(today-birthdate) < 10 AND id IN (:users_id)', { user_id })
+					.where('extract(day from NOW()-birthdate) < 3650 AND id IN (:...users_id)', {
+						users_id,
+					})
 					.getCount();
+
+				// console.log(ten);
 				const thirteen = await userRepository
 					.createQueryBuilder('user')
 					.select('birthdate')
 					.where(
-						'(today-birthdate) > 10 AND (today-birthdate) <= 13  AND id IN (:users_id)',
-						{ user_id },
+						'extract(day from NOW()-birthdate) >= 3650 AND extract(day from NOW()-birthdate) < 5110 AND id IN (:...users_id)',
+						{
+							users_id,
+						},
 					)
 					.getCount();
 				const sixteen = await userRepository
 					.createQueryBuilder('user')
 					.select('birthdate')
 					.where(
-						'(today-birthdate) > 13 AND (today-birthdate) < 17 AND id IN (:users_id)',
-						{ user_id },
+						'extract(day from NOW()-birthdate) >= 5110 AND extract(day from NOW()-birthdate) < 6205 AND id IN (:...users_id)',
+						{
+							users_id,
+						},
 					)
 					.getCount();
 				const nineteen = await userRepository
 					.createQueryBuilder('user')
 					.select('birthdate')
 					.where(
-						'(today-birthdate) > 16 AND (today-birthdate) < 20 AND id IN (:users_id)',
-						{ user_id },
+						'extract(day from NOW()-birthdate) >= 6205 AND extract(day from NOW()-birthdate) < 7300 AND id IN (:...users_id)',
+						{
+							users_id,
+						},
 					)
 					.getCount();
 				const tw_four = await userRepository
 					.createQueryBuilder('user')
 					.select('birthdate')
 					.where(
-						'(today-birthdate) >19 10 AND (today-birthdate) < 25 AND id IN (:users_id)',
-						{ user_id },
+						'extract(day from NOW()-birthdate) >= 7300 AND extract(day from NOW()-birthdate) < 8760 AND id IN (:...users_id)',
+						{
+							users_id,
+						},
 					)
 					.getCount();
 				const tw_five = await userRepository
 					.createQueryBuilder('user')
 					.select('birthdate')
-					.where('(today-birthdate) >25 10 AND id IN (:users_id)', { user_id })
+					.where('extract(day from NOW()-birthdate) >= 8760 AND id IN (:...users_id)', {
+						users_id,
+					})
 					.getCount();
 
-				return { status };
+				const subs = await subsRepo.find({
+					select: ['user_subscriber'],
+					where: { user_target: user_id },
+				});
+
+				var subs_id = new Array();
+				for (let i = 0; i < subs.length; i++) {
+					subs_id.push(subs[i].user_subscriber);
+				}
+
+				//SELECT target_id, COUNT(asterisco) FROM subscriptions WHERE origin_id IN (...) ORDER BY COUNT(asterisco) DESC LIMIT 10
+
+				const channels = await subsRepo
+					.createQueryBuilder('subscriptions')
+					.select('user_target')
+					.addSelect('COUNT(*)')
+					.where('user_subscriber IN (:...subs_id)', { subs_id })
+					.groupBy('user_target')
+					.orderBy('COUNT(*)', 'DESC')
+					.limit(10)
+					.getRawMany();
+
+				var users = new Array();
+				for (let i = 0; i < channels.length; i++) {
+					users.push(channels[i].user_target);
+				}
+
+				const infos_user = await userRepository
+					.createQueryBuilder('user')
+					.select('username')
+					.addSelect('id')
+					.where('id IN (:...users)', { users })
+					.getRawMany();
+
+				var countUser = new Array(infos_user.length);
+				for (let i = 0; i < users.length; i++) {
+					for (let j = 0; j < infos_user.length; j++) {
+						if (channels[i].user_target === infos_user[j].id) {
+							countUser[j] = channels[i].count;
+						}
+					}
+				}
+				var data = {
+					gender: [female, male, others],
+					age: [ten, thirteen, sixteen, nineteen, tw_four, tw_five],
+					channels: {
+						name: infos_user,
+						data: countUser,
+					},
+				};
+				return data;
 			} else {
 				throw new Error('Erro ao resgatar repositório.');
 			}
